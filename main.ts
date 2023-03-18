d549477a243e9e74378a9609683d1f25582d1002
Simon <125518073+theProgrammer-Best@users.noreply.github.com>
2023-03-18 17:43:18 -0500

    I made it automatic and cooler (in my opinion)

--- main.ts
+++ main.ts
@@ -1,20 +1,41 @@
+function rainbowConvert (num: number) {
+    if (num < 1) {
+        return 2
+    } else if (num < 2) {
+        return 4
+    } else if (num < 3) {
+        return 5
+    } else if (num < 4) {
+        return 7
+    } else if (num < 5) {
+        return 8
+    } else if (num < 6) {
+        return 9
+    } else {
+        return 10
+    }
+}
 let num = 0
+let picture = 0
 let primeList: number[] = []
 forever(function () {
+    picture = 0
+    music.stopAllSounds()
     num += 1
     for (let value of primeList) {
         if (num % value == 0) {
-            scene.backgroundImage().setPixel(159, primeList.indexOf(value), 1)
+            scene.backgroundImage().setPixel(159, primeList.indexOf(value), rainbowConvert(num % 7))
             music.play(music.createSoundEffect(
             WaveShape.Square,
             2000 / value,
             2000 / value,
             255,
             255,
-            200,
+            10000,
             SoundExpressionEffect.None,
             InterpolationCurve.Linear
             ), music.PlaybackMode.InBackground)
+            picture += 1
         }
     }
     for (let index = 0; index <= 159; index++) {
@@ -24,6 +45,5 @@ forever(function () {
         }
     }
     primeList.push(num)
-    pause(100)
-    music.stopAllSounds()
+    pause(picture ** 2 * (Math.sqrt(num) * 2))
 })
